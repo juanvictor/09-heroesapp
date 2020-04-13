07520ce12168dc2c982e0d4c40cda582909e568a
JUAN VICTOR SERRUDO CHAVEZ <juan.vsc@gmail.com>
2020-04-13 19:19:30 -0400

    Clase 221. Información del estado de las peticiones.

--- src/app/pages/heroe/heroe.component.ts
+++ src/app/pages/heroe/heroe.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroeModel } from '../../models/heroe.model';
 import { NgForm } from '@angular/forms';
+import { Observable } from 'rxjs';
+
+import Swal from 'sweetalert2';
 
 import { HeroesService } from '../../services/heroes.service';
 
@@ -28,17 +31,39 @@ export class HeroeComponent implements OnInit {
     // console.log(form);
     // console.log(this.heroe);
 
+    Swal.fire({
+      title: 'Espere',
+      text: 'Guardando información',
+      icon: 'info',
+      allowOutsideClick: false
+    });
+    Swal.showLoading();
+
+    let peticion: Observable<any>;
+
     if ( this.heroe.id ) {
-      this.heroesService.actualizarHeroe( this.heroe )
-        .subscribe( resp => {
-          console.log(resp);
-        });
+      peticion = this.heroesService.actualizarHeroe( this.heroe );
+
+      // this.heroesService.actualizarHeroe( this.heroe )
+      //   .subscribe( resp => {
+      //     console.log(resp);
+      //   });
     } else {
-      this.heroesService.crearHeroe( this.heroe )
-        .subscribe( resp => {
-          console.log(resp);
-        });
+      peticion = this.heroesService.crearHeroe( this.heroe );
+
+      // this.heroesService.crearHeroe( this.heroe )
+      //   .subscribe( resp => {
+      //     console.log(resp);
+      //   });
     }
+
+    peticion.subscribe( resp => {
+      Swal.fire({
+        title: this.heroe.nombre,
+        text: 'Se actualizó correctamente',
+        icon: 'success'
+      });
+    });
   }
 
 }
